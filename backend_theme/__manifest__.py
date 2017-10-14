fb685894549871c7338c5096f9bcec69fc79003c
Hugo Rodrigues <me@hugorodrigues.net>
2017-10-14 21:42:20 +0100

    [REF] Updated information
    Updated module description
    Fixed some issues inside the manifest (like mixed tabs and staces)
    
    Signed-off-by: Hugo Rodrigues <me@hugorodrigues.net>

--- backend_theme/__manifest__.py
+++ backend_theme/__manifest__.py
@@ -1,27 +1,32 @@
 # -*- coding: utf-8 -*-
+"""Module information"""
 # Copyright 2016, 2017 Openworx
+# Copyright 2017 Hugo Rodrigues
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# pylint: disable=pointless-statement
 
 {
     "name": "Material/United Backend Theme",
     "summary": "Odoo 11.0 community backend theme",
     "version": "11.0.1.0.0",
     "category": "Themes/Backend",
-    "website": "http://www.openworx.nl",
-	"description": """
-		Backend theme for Odoo 11.0 community edition.
-    """,
-	'images':[
-        'images/screen.png'
-	],
-    "author": "Openworx",
+    "website": "https://hugorodrigues.net",
+    "description": """
+        Backend theme for Odoo 11.0 community edition.
+        """,
+    "images": [
+        "images/screen.png"
+    ],
+    "author": "Openworx, Hugo Rodrigues",
     "license": "LGPL-3",
     "installable": True,
     "depends": [
-	'web_responsive',
+        'web_responsive',
+    ],
+    "excludes": [
+        'web_enterprise'
     ],
     "data": [
         'views/assets.xml',
     ],
 }
-
